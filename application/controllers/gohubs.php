3c64cff288e0e4764d11d2a053fc77900281ead3
Aaris-Kazi <aariskazi@gmail.com>
2020-11-26 14:20:12 +0530

    The first MVC

---
+++ application/controllers/gohubs.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Gohubs extends CI_Controller{
+    public function index(){
+        // $conn = 'mysql:host=localhost;dbname=static_pages';
+        // $this->load->database($conn);
+        $this->load->helper('url');
+        $this->load->view('head');
+        $this->load->view('index');
+    }
+    public function login(){
+        $this->load->helper('url');
+        $this->load->view('head');
+        $this->load->view('login');
+    }
+    public function logout(){
+        $this->load->helper('url');
+        $this->load->view('head');
+        $this->load->view('logout');
+    }
+    public function register(){
+        $this->load->helper('url');
+        $this->load->view('head');
+        $this->load->view('register');
+    }
+    public function search(){
+        $this->load->helper('url');
+        $this->load->view('head');
+        $this->load->view('search');
+    }
+}
+
+?>
